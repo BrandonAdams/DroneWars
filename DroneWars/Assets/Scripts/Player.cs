3fefd302cbd1c6d6e865d834097b3d7ed8bb54da
BrandonAdams <nachmir@Live.com>
2014-05-01 16:55:23 -0400

    Updated Movement to calculate all forces

--- DroneWars/Assets/Scripts/Player.cs
+++ DroneWars/Assets/Scripts/Player.cs
@@ -3,36 +3,47 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	public float speed = 0.0f;
+	/****   VARIABLES FOR MOVEMENT  ******/
 	public float strafeSpeed = 0.0f;
 	public float liftSpeed = 0.0f;
 	public float declSpeed = 0.95f;
 	public float speedMod = 0.05f;
 	
 	public float rotationSpeed = 5.0f;
-	public float maxSpeed = 2.0f;
 	public float maxLift = 3.0f;
 	public float tiltSpeed = 1.0f;
-	
-	
+	private bool isFalling = false;
+	private bool canFly = true;	
 	private float timer = 0;
-	
-	public GameObject cam;
-	
-	private bool isGravOn = false;
-	private bool canFly = true;
-	
-	// The initial orientation.
-	private Quaternion initialOrientation;
-	
+
+	//The Character Controller attached to this gameObject
+	CharacterController characterController;	
+	// The linear gravity factor. Made available in the Editor.
+	public float gravity = 100.0f;	
+	// mass of vehicle
+	public float mass = 1.0f;	
 	// The cummulative rotation about the y and x-Axis.
 	private float cummulativeRotationYAxis;
 	private float cummulativeRotationXAxis;	
-	
 	// The rotation factor, this will control the speed we rotate at.
-	public float rotationSensitvity = 500.0f;
-	private float maxUpRollRotation = -300;
-	private float maxDownRollRotation = -430;
+	public float rotationSensitvity = 200.0f;
+	private float maxUpRollRotation = 35;
+	private float maxDownRollRotation = -35;
+	
+	//movement variables - exposed in inspector panel
+	public float maxSpeed = 2.0f; 		//maximum speed of vehicle
+	public float maxForce = 15.0f; 		// maximimum force allowed
+	public float friction = 0.997f; 	// multiplier decreases speed
+	
+	//movement variables - updated by this component
+	private float speed = 0.0f;  		//current speed of vehicle
+	private Vector3 steeringForce; 		// force that accelerates the vehicle
+	private Vector3 velocity; 			//change in position per second
+	/**************************************/
+
+	public GameObject cam;	
+	// The initial orientation.
+	private Quaternion initialOrientation;
 
 	public GameObject nameDisplay;
 	private string playerName;
@@ -91,6 +102,7 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+		//DISPLAYING THE PLAYERS NAME ON THE SCREEN
 		nameDisplay.GetComponent<TextMesh>().text = playerName;
 		nameDisplay.transform.position = new Vector3(transform.position.x, transform.position.y - 40, transform.position.z);
 
@@ -98,14 +110,18 @@ public class Player : MonoBehaviour {
 		{
 
 			/*if(_isGameOver)
-		{
-			
-		}*/
+			{
+				
+			}*/
 			if(_isGameStarted)
 			{ 		
 				//players = GameObject.FindGameObjectsWithTag("Drone");
 				checkKeyDown();
 				checkKeyUp();
+
+				SteerWithMouse();
+				// calculate steering forces that will change our position in space
+				transform.position += CalcForces();
 
 				if(_isFiringPrimary)
 				{
@@ -141,167 +157,23 @@ public class Player : MonoBehaviour {
 				}
 				//Increase the timer for the missle
 				_missleFiringTimer++;
-				
+
+								
 			}
 
-			SteerWithMouse();
-			
-			updateMovement();
+
 		}
 
 	}
 
-	void SteerWithMouse ()
-	{
+	private Vector3 KeyboardAcceleration ()
+	{	
+		Vector3 force;		
+		//dv is desired velocity
+		Vector3 dv = Vector3.zero;
 
-
-		//Get the left/right Input from the Mouse and use time along with a scaling factor 
-		// to add a controlled amount to our cummulative rotation about the y-Axis.
-		cummulativeRotationYAxis += Input.GetAxis ("Mouse X") * Time.deltaTime * rotationSensitvity;
-		cummulativeRotationXAxis -= Input.GetAxis ("Mouse Y") * Time.deltaTime * rotationSensitvity;	
-		
-		clampXRotation ();
-		
-		//Create a Quaternion representing our current cummulative rotation around the y-axis. 
-		Quaternion currentRotation = Quaternion.Euler (cummulativeRotationXAxis, cummulativeRotationYAxis, 0.0f);
-		
-		// Use the quaternion to update the transform of the vehicle's Game Object based on 
-		// initial orientation and the accumulated rotation since the original orientation. 
-		transform.rotation = initialOrientation * currentRotation;
-		
-	}
-
-	void checkKeyDown()
-	{
-		if(Input.GetKeyDown (KeyCode.Mouse0))
-		{
-			_isFiringPrimary = true;
-			whirringBladesObject1.audio.mute = true;
-			whirringBladesObject2.audio.mute = true;
-			
-		}
-		
-		if(Input.GetKeyDown (KeyCode.Mouse1))
-		{
-			
-			Transform target = this.transform;
-			//Raycast -should be what we use or some other construct
-			
-			//For now ill just take the first player that connects to you
-			if(this.GetComponent<PlayerManager>().Players.Count > 0)
-			{
-				target = this.GetComponent<PlayerManager>().Players[0].transform;
-			}
-			
-			
-			
-			//Fire secondary weapon here 
-			if(_missleFiringTimer > _missleFiringTime)
-			{
-				//create a bullet and place it just in front of the player
-				GameObject myPlayer = GameObject.Find(_myView.observed.name);
-				Vector3 missleSpawnPosition = myPlayer.transform.position; 
-				missleSpawnPosition += myPlayer.transform.forward * 8;
-				Missle r = (Missle)Network.Instantiate(missle, missleSpawnPosition, myPlayer.transform.rotation, 1);
-				networkView.RPC("initiateMissile", RPCMode.AllBuffered, r.networkView.viewID, target.gameObject.networkView.viewID);
-				//r.Initialize(.001f, 50, target.gameObject.networkView.viewID, _myView.observed.name, 1);
-				//reset the firing timer
-				_missleFiringTimer = 0;
-				_missleCounter++;				
-			}
-		}
-		
-	}
-	
-	[RPC]
-	void initiateMissile(NetworkViewID missile, NetworkViewID target) {
-		
-		GameObject m = NetworkView.Find(missile).observed.gameObject;
-		m.GetComponent<Missle>().Initialize(.001f, 50, target, _myView.observed.name, 1);
-		
-	}
-	
-	
-	void checkKeyUp()
-	{
-		if(Input.GetKeyUp (KeyCode.Mouse0))
-		{
-			_isFiringPrimary = false;
-			whirringBladesObject1.audio.mute = false;
-			
-		}
-	}
-	
-	void checkForWinningPlayer(GameObject[] playerList)
-	{		
-		
-		
-	}	
-	
-	/**
-     * A method to check if a non it player is close to a player that is it
-     **/
-	void checkPlayerDistances(GameObject[] playerList)
-	{
-		//go through the list of players and check to see which player is me
-		for(int i=0; i < playerList.Length; i++)
-		{
-			//GameObject thePlayer = playerList[i];
-			NetworkViewID theID = playerList[i].networkView.viewID;
-			NetworkView theView = NetworkView.Find(theID);
-			
-			//If the network view is mine then check myself against all other player in vicinity
-			if(theView.isMine)
-			{
-				//go through the list of players and check my distance from them
-				for(int j=0; j < playerList.Length; j++)
-				{
-					NetworkViewID playerID = playerList[i].networkView.viewID;
-					NetworkView playerView = NetworkView.Find(playerID);
-					if (playerView.isMine)
-						continue; //don't check the distance against myself
-					else{
-						
-					}                   
-				}
-				break; //don't continue to search for me in the list since I have been found.
-			}		
-		}
-	}//END checkPlayerDistances
-
-	/*void OnCollisionEnter(Collision collision)
-	{
-		if(collision.gameObject != this)
-			Debug.Log("hit something:" + collision.gameObject.name);// modify this to stop movement
-	}*/
-
-	void clampXRotation()
-	{
-		if(cummulativeRotationXAxis > maxUpRollRotation)
-		{
-			cummulativeRotationXAxis = maxUpRollRotation;
-		}		
-		else if(cummulativeRotationXAxis < maxDownRollRotation)
-		{
-			cummulativeRotationXAxis = maxDownRollRotation;
-		} 
-		
-	}
-
-
-
-	void updateMovement()
-	{
-		
 		if(canFly == true)
 		{
-			#region arrows
-			#endregion
-			
-			#region Movement
-			
-			
-			#region W
 			if(Input.GetKey (KeyCode.W) && !(Input.GetKey (KeyCode.S)))
 			{
 				if(speed < maxSpeed)
@@ -310,8 +182,7 @@ public class Player : MonoBehaviour {
 				}
 				else 
 				{
-					speed = maxSpeed;
-					
+					speed = maxSpeed;					
 				}
 			}
 			else if(!(Input.GetKey (KeyCode.W)) && speed> 0)
@@ -321,10 +192,8 @@ public class Player : MonoBehaviour {
 				{
 					speed = 0;
 				}
-			}
-			#endregion W
+			}			
 			
-			#region S
 			if(Input.GetKey (KeyCode.S) && !(Input.GetKey (KeyCode.W)))
 			{
 				if(speed > -maxSpeed)
@@ -344,13 +213,7 @@ public class Player : MonoBehaviour {
 					speed = 0;
 				}
 			}
-			#endregion S
 			
-			
-			#region A&D
-			
-			#endregion
-			#region D
 			if((Input.GetKey (KeyCode.D)) && !(Input.GetKey (KeyCode.A)))
 			{
 				if(strafeSpeed < maxSpeed)
@@ -371,8 +234,8 @@ public class Player : MonoBehaviour {
 					strafeSpeed = 0;
 				}
 			}
-			#endregion
-			#region A
+			
+			
 			if((Input.GetKey (KeyCode.A)) && !(Input.GetKey (KeyCode.D)))
 			{
 				if(strafeSpeed > -maxSpeed)
@@ -384,8 +247,7 @@ public class Player : MonoBehaviour {
 				{
 					strafeSpeed = -maxSpeed;
 					
-				}
-				
+				}				
 			}
 			else if(!(Input.GetKey (KeyCode.A)) && strafeSpeed < 0)
 			{
@@ -395,12 +257,7 @@ public class Player : MonoBehaviour {
 					strafeSpeed = 0;
 				}
 			}
-			#endregion
 			
-			
-			#region Space and Shift
-			#endregion
-			#region Space
 			if(Input.GetKey( KeyCode.Space) && !(Input.GetKey (KeyCode.LeftShift)))
 			{
 				
@@ -420,10 +277,8 @@ public class Player : MonoBehaviour {
 				{
 					liftSpeed = 0;
 				}
-			}
+			}			
 			
-			#endregion
-			#region Shift
 			if(Input.GetKey(KeyCode.LeftShift) && !(Input.GetKey (KeyCode.Space)))
 			{
 				if(liftSpeed > -maxLift)
@@ -462,46 +317,14 @@ public class Player : MonoBehaviour {
 				}
 				else
 					liftSpeed *= declSpeed;
-			}
-			#endregion
-			
-			
-			
-			#endregion Movement
-			
-			
+			}			
 		}
-		
-		#region checkGrav
-		if(Input.GetKeyDown (KeyCode.F))
-		{ 
-			if(isGravOn == false)
-			{
-				isGravOn = true;
-				canFly = false;
-				
-				Debug.Log (isGravOn);
-			}
-			else if(isGravOn == true)
-			{
-				isGravOn = false;
-				Debug.Log (isGravOn);
-			}
-		}
-		#endregion
-		#region gravOn
-		if(isGravOn == true)
+
+		#region Drone shut down parameters
+		/***** AUGMENTING OUR SPEED VARIABLES IF THE DRONE IS SHUT DOWN ******/
+		if(isFalling)
 		{
-			if(speed < 0)
-			{
-				speed += speedMod;
-				
-			}
-			else if (speed > 0)
-			{
-				speed -= speedMod;
-				
-			}
+			Debug.Log("Player is Falling");
 			if(liftSpeed < 0)
 			{
 				liftSpeed += speedMod;
@@ -526,38 +349,186 @@ public class Player : MonoBehaviour {
 			
 			timer++;
 		}
-		#endregion
-		#region gravOff
-		if(isGravOn == false)
+		else if(isFalling == false)
 		{
-			if(timer >= 0)
+			Debug.Log("Player is not Falling");
+			if(timer > 0)
 			{
 				timer -= 2;
 				liftSpeed += ((timer/60) * -0.05f);
 			}
 			else{
 				canFly = true;
+				timer = 0;
 			}
 		}
-		#endregion
-		
-		
-		//Vector3 moveForward = new Vector3();
-		//moveForward.x = transform.forward.x;
-		//moveForward.z = transform.forward.z;
-		//transform.forward *= speed;
-		transform.position += transform.forward * speed;
-		//this.rigidbody.AddForce(transform.forward * speed, ForceMode.Impulse);// if not kinematic (kinematic will tell you collisions not act on them)
+		#endregion Drone shut down parameters
 
-		//Vector3 strafe = new Vector3();
-		//strafe.x = transform.right.x;
-		//strafe.z = transform.right.z;
-		//strafe.Normalize();
-		transform.position += transform.right *strafeSpeed;
+		Vector3 planeVector = Vector3.zero;
+		planeVector.x = this.transform.forward.x;
+		planeVector.z = this.transform.forward.z;
+		planeVector *= speed;
+		dv += planeVector;
+		//transform.position += transform.forward * speed;
 		
-		//Vector3 lift = new Vector3();
-		//lift.y = transform.up.y;
-		//lift.Normalize();
-		transform.position += transform.up * liftSpeed;
+		Vector3 strafeVector = new Vector3();
+		strafeVector.x = transform.right.x;
+		strafeVector.z = transform.right.z;
+		strafeVector.Normalize();
+		strafeVector *= strafeSpeed;
+		dv += strafeVector;
+		//transform.position += transform.right *strafeSpeed;
+		
+		Vector3 liftVector = new Vector3();
+		liftVector.y = transform.up.y;
+		liftVector.Normalize();
+		liftVector *= liftSpeed;
+
+		dv += liftVector; 
+		//transform.position += transform.up * liftSpeed;
+				
+		return dv;
 	}
+	
+	// Calculate the forces that alter velocity
+	private Vector3 CalcForces ()
+	{
+		steeringForce = Vector3.zero;
+		steeringForce += KeyboardAcceleration ();
+
+		return steeringForce;		
+	}
+	
+	// if steering forces exceed maxForce they are set to maxForce
+	private void ClampForces ()
+	{
+		if (steeringForce.magnitude > maxForce) {
+			steeringForce.Normalize ();
+			steeringForce *= maxForce;
+		}
+	}
+
+	void SteerWithMouse ()
+	{
+
+
+		//Get the left/right Input from the Mouse and use time along with a scaling factor 
+		// to add a controlled amount to our cummulative rotation about the axis'.
+		cummulativeRotationYAxis += Input.GetAxis ("Mouse X") * Time.deltaTime * rotationSensitvity;
+		cummulativeRotationXAxis -= Input.GetAxis ("Mouse Y") * Time.deltaTime * rotationSensitvity;	
+		//CLAMP THE X ROTATION TO PREVENT IT FROM LOOKING TOO FAR
+		if(cummulativeRotationXAxis > maxUpRollRotation)
+		{
+			cummulativeRotationXAxis = maxUpRollRotation;
+		}		
+		else if(cummulativeRotationXAxis < maxDownRollRotation)
+		{
+			cummulativeRotationXAxis = maxDownRollRotation;
+		}
+
+		
+		//Create a Quaternion representing our current cummulative rotation around the y-axis. 
+		Quaternion currentRotation = Quaternion.Euler (cummulativeRotationXAxis, cummulativeRotationYAxis, 0.0f);
+		
+		/** Use the quaternion to update the transform of the vehicle's Game Object based on 
+		 initial orientation and the accumulated rotation since the original orientation. **/
+		transform.rotation = initialOrientation * currentRotation;
+		
+	}
+
+	void checkKeyDown()
+	{
+		if(Input.GetKeyDown (KeyCode.Mouse0))
+		{
+			_isFiringPrimary = true;		
+		}
+		
+		if(Input.GetKeyDown (KeyCode.Mouse1))
+		{
+			
+			Transform target = this.transform;
+			//Raycast -should be what we use or some other construct
+			
+			//For now ill just take the first player that connects to you
+			if(this.GetComponent<PlayerManager>().Players.Count > 0)
+			{
+				target = this.GetComponent<PlayerManager>().Players[0].transform;
+			}
+
+			
+			//Fire secondary weapon here 
+			if(_missleFiringTimer > _missleFiringTime)
+			{
+				//create a bullet and place it just in front of the player
+				GameObject myPlayer = GameObject.Find(_myView.observed.name);
+				Vector3 missleSpawnPosition = myPlayer.transform.position; 
+				missleSpawnPosition += myPlayer.transform.forward * 8;
+				Missle r = (Missle)Network.Instantiate(missle, missleSpawnPosition, myPlayer.transform.rotation, 1);
+				networkView.RPC("initiateMissile", RPCMode.AllBuffered, r.networkView.viewID, target.gameObject.networkView.viewID);
+				//r.Initialize(.001f, 50, target.gameObject.networkView.viewID, _myView.observed.name, 1);
+				//reset the firing timer
+				_missleFiringTimer = 0;
+				_missleCounter++;				
+			}
+		}
+
+		if(Input.GetKeyDown (KeyCode.F))
+		{ 
+			isFalling = !isFalling;
+			if(!isFalling)
+				canFly = false;
+		}
+		
+	}
+
+	void checkKeyUp()
+	{
+		if(Input.GetKeyUp (KeyCode.Mouse0))
+		{
+			_isFiringPrimary = false;
+			whirringBladesObject1.audio.mute = false;
+			
+		}
+	}	
+
+	
+	/**
+     * A method to check if a non it player is close to a player that is it
+     **/
+	void checkPlayerDistances(GameObject[] playerList)
+	{
+		//go through the list of players and check to see which player is me
+		for(int i=0; i < playerList.Length; i++)
+		{
+			//GameObject thePlayer = playerList[i];
+			NetworkViewID theID = playerList[i].networkView.viewID;
+			NetworkView theView = NetworkView.Find(theID);
+			
+			//If the network view is mine then check myself against all other player in vicinity
+			if(theView.isMine)
+			{
+				//go through the list of players and check my distance from them
+				for(int j=0; j < playerList.Length; j++)
+				{
+					NetworkViewID playerID = playerList[i].networkView.viewID;
+					NetworkView playerView = NetworkView.Find(playerID);
+					if (playerView.isMine)
+						continue; //don't check the distance against myself
+					else{
+						
+					}                   
+				}
+				break; //don't continue to search for me in the list since I have been found.
+			}		
+		}
+	}//END checkPlayerDistances
+
+	[RPC]
+	void initiateMissile(NetworkViewID missile, NetworkViewID target) {
+		
+		GameObject m = NetworkView.Find(missile).observed.gameObject;
+		m.GetComponent<Missle>().Initialize(.001f, 50, target, _myView.observed.name, 1);
+		
+	}
+
 }
